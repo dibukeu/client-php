aa040a5c1bff6989b1c761f30311361af23befb7
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-01-15 15:09:26 +0100

    test call request

--- test/ChangeOwnershipTest.php
+++ test/ChangeOwnershipTest.php
@@ -2,13 +2,10 @@
 
 namespace DibukEu\Test;
 
+use DibukEu\DibukClient;
+
 class ChangeOwnershipTest extends DibukTestCase
 {
-
-    /** @var int */
-    private $bookId = 50;
-    /** @var int */
-    private $userId = 101;
 
     /**
      * @return void
@@ -23,9 +20,38 @@ class ChangeOwnershipTest extends DibukTestCase
     }
 
     /**
-     * @return void
      * @throws \Exception
-     * @expectedException \RuntimeException
+     */
+    public function testCallApi(): void
+    {
+        $this->dibukClient = $this->getMockBuilder(DibukClient::class)
+            ->setConstructorArgs(
+                [[
+                    'signature' => 'test_secret',
+                    'sellerId' => 1,
+                    'version' => '2.3',
+                    'url' => 'api.dibuk.lsk',
+                ]]
+            )
+            ->setMethods(['call'])->getMock();
+        $this->dibukClient->expects($this->once())
+            ->method('call')
+            ->with(
+                'changeOwnership', [
+                    'book_id' => 50,
+                    'user_id' => 101,
+                    'new_user_id' => 202,
+                ]
+            )->willReturn(['status' => DibukClient::STATUS_OK]);
+
+        $this->setValidClient();
+        $this->assertTrue($this->dibukClient->changeOwnership());
+    }
+
+    /**
+     * @return                   void
+     * @throws                   \Exception
+     * @expectedException        \RuntimeException
      * @expectedExceptionMessage Dibuk changeOwnership call failed with response {"status":"ERR"}
      */
     public function testResponseError(): void
@@ -41,6 +67,7 @@ class ChangeOwnershipTest extends DibukTestCase
 
     /**
      * set up valid client
+     *
      * @return void
      */
     private function setValidClient(): void
